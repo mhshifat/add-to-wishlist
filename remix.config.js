3593e6b12f92f76d834479fa768ccb319a223250
mhshifat <mehedihassanshifat01@gmail.com>
2024-09-21 20:05:55 +0600

    updated remix config

--- remix.config.js
+++ remix.config.js
@@ -1,15 +1,28 @@
+if (
+  process.env.HOST &&
+  (!process.env.SHOPIFY_APP_URL ||
+    process.env.SHOPIFY_APP_URL === process.env.HOST)
+) {
+  process.env.SHOPIFY_APP_URL = process.env.HOST;
+  delete process.env.HOST;
+}
+
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
+  ignoredRouteFiles: ["**/.*"],
   appDirectory: "app",
-  assetsBuildDirectory: "public/build",
-  publicPath: "/build/",
-  serverBuildPath: "build/index.js",
-  ignoredRouteFiles: ["**/*.css"],
+  serverModuleFormat: "cjs",
+  future: {
+    v2_errorBoundary: true,
+    v2_headers: true,
+    v2_meta: true,
+    v2_normalizeFormMethod: true,
+    v2_routeConvention: true,
+    v2_dev: {
+      port: process.env.PORT || 8002,
+    },
+  },
   serverDependenciesToBundle: [
     /^remix-utils.*/,
-    /^rehype.*/,
-    /^remark.*/,
-    /^unified.*/,
-    "@sindresorhus/slugify",
   ],
 };
